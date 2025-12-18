38d2e5b0376da7c39514c70038b5d393f2324582
Alicja Kot <wymyslony.mail@blabla.pl>
2025-12-18 22:15:47 +0100

    Modification 3

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,6 +1,11 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
+    private String name;
+
+    public void sniff() {
+        System.out.println("I'm soo sniffing!");
+    }
 
     @Override
     public String whatIsYourName() {
